2a34eec3259060c49c1f2b3a77283ab6ff7c6104
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2023-12-01 14:35:22 -0700

    Solves the meal js test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,56 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 
 }
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+}
+return menuItem
+}
+
+function addIngredients(toppings, ingredients) {
+  if (!ingredients.includes(toppings)) {
+ ingredients.push(toppings);
+ }
+}
+
+function formatPrice(initialPrice){
+  var formatPrice = "$" + initialPrice
+  return formatPrice
+}
+
+function decreasePrice(decreasedPrice) {
+var decreasedPrice = decreasedPrice * 0.9
+return decreasedPrice
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+  return recipe
+  
+ 
+
+}
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
+
 
 
